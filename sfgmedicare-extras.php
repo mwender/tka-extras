e7aa41ad32a29eee93da66da011c9e9a91ec879e
Michael Wender <michael@michaelwender.com>
2021-06-14 14:30:07 -0400

    First commit

---
+++ sfgmedicare-extras.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     SFG Medicare Extras
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Various extensions for the SFG Medicare website
+ * Author:          TheWebist
+ * Author URI:      https://mwender.com
+ * Text Domain:     sfgmedicare-extras
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Sfgmedicare_Extras
+ */
+
+// Your code starts here.
