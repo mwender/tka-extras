d89f2b8f228e8bd80e3a0ae229340e83aed247c9
Michael Wender <michael@michaelwender.com>
2021-07-19 09:58:34 -0400

    Checking for func existence

--- sfgmedicare-extras.php
+++ sfgmedicare-extras.php
@@ -26,14 +26,17 @@ require_once( SFG_PLUGIN_PATH . 'lib/fns/utilities.php' );
  *
  * @param      string  $message  The log message
  */
-function uber_log( $message = null ){
-  static $counter = 1;
+if( ! function_exists( 'uber_log' ) ){
+  function uber_log( $message = null ){
+    static $counter = 1;
 
-  $bt = debug_backtrace();
-  $caller = array_shift( $bt );
+    $bt = debug_backtrace();
+    $caller = array_shift( $bt );
 
-  if( 1 == $counter )
-    error_log( "\n\n" . str_repeat('-', 25 ) . ' STARTING DEBUG [' . date('h:i:sa', current_time('timestamp') ) . '] ' . str_repeat('-', 25 ) . "\n\n" );
-  error_log( "\n" . $counter . '. ' . basename( $caller['file'] ) . '::' . $caller['line'] . "\n" . $message . "\n---\n" );
-  $counter++;
+    if( 1 == $counter )
+      error_log( "\n\n" . str_repeat('-', 25 ) . ' STARTING DEBUG [' . date('h:i:sa', current_time('timestamp') ) . '] ' . str_repeat('-', 25 ) . "\n\n" );
+    error_log( "\n" . $counter . '. ' . basename( $caller['file'] ) . '::' . $caller['line'] . "\n" . $message . "\n---\n" );
+    $counter++;
+  }
 }
+
