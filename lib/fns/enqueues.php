5d88d9fc2212b2e98fb7421b3b7e48ce962723ba
Michael Wender <michael@michaelwender.com>
2021-07-20 10:56:32 -0400

    Adding CSS

--- lib/fns/enqueues.php
+++ lib/fns/enqueues.php
@@ -2,6 +2,14 @@
 
 namespace sfgmedicare\enqueues;
 
+function enqueue_scripts(){
+  // Our custom styles
+  $css_filename = SFG_PLUGIN_PATH . 'lib/' . SFG_CSS_DIR . '/main.css';
+  if( file_exists( $css_filename ) )
+    wp_enqueue_style( 'sfgmedicare', SFG_PLUGIN_URL . 'lib/' . SFG_CSS_DIR . '/main.css', ['hello-elementor','elementor-frontend'], filemtime( $css_filename ) );
+}
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts' );
+
 /**
  * Custom styles for the WP Admin
  */
